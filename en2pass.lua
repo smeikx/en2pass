2fe07484884f2c907e5d7f751bdde3378c37d437
smeikx <smeikx@users.noreply.github.com>
2020-08-06 20:15:49 +0200

    initial commit

---
+++ en2pass.lua
@@ -0,0 +1,63 @@
+#!/usr/bin/env lua
+
+-- XXX REQUIRES LUA 5.4 (but can easily be adapted for older versions)
+-- most probably won’t run on Windows
+
+-- This script feeds a password database exported from Enpass in plain text to ‘pass’.
+-- It takes a plain text file and assumes that each entry is separated by two blank lines.
+
+-- Example Input
+--[[
+password : super-secret-password
+title : My Favourite Website
+url : https://gentoo.org/
+username : xyz123foobar
+
+
+email : what@ever.est
+title : IDK
+url : https://kernel.org/
+password : you won't guess it
+note: i luv it
+
+
+]]--
+-- XXX The two trailing blank lines are necessary to catch the last entry.
+
+-- The name of the input file will be used as the name of the subdirectory in pass.
+-- A field with the label ‘title’ will be used as the name of the output file.
+-- The first line of each pass entry will contain the password, the rest comes afterwards.
+
+-- an array containing tables with the keys ‘title’, ‘password’, ‘content’ (the other fields as string)
+local entries <const> = {}
+
+-- the content of the input file as one string
+local content = ''
+
+do
+	local file <close> = assert(io.open(arg[1], 'r'))
+	content = file:read('a')
+end
+
+for entry in string.gmatch(content, '(.-)\n\n\n') do
+	entries[#entries+1] = {
+		title = string.match(entry, 'title ?: ([^\n]+)\n'),
+		password = string.match(entry, 'password ?: ([^\n]+)\n'),
+		content = ''
+	}
+	local content <const> = {}
+	for line in string.gmatch(entry, '([^\n]+)') do
+		if not string.find(line, '^title:') and not string.find(line, '^password:') then
+			content[#content+1] = line
+		end
+	end
+	entries[#entries].content = table.concat(content, '\n')
+end
+
+local sub_dir = string.match(arg[1], '.*/(.+)%.')..'/'
+for _,entry in ipairs(entries) do
+	local pass <close> = assert(io.popen('pass insert -fm "'..sub_dir..entry.title..'"', 'w'), 'pass not working …')
+	--local pass = io.stdout -- a dry run for debugging – prints passwords to stdout!
+	if entry.password then pass:write(entry.password..'\n') end
+	pass:write(entry.content)
+end
